89882d55c6b052345a26769f4ea62ee3a737b3b5
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-18 23:24:08 +0300

    comleted

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speaker_name_array)
+    speaker_name_array.map do |speaker_name|
+         "Hello, my name is #{speaker_name}."
+    end
+end
+
+def assign_rooms (speakers)
+    speakers.map.with_index(1) do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+end
+def printer (speakers)
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+    assign_rooms(speakers).each do |room|
+        puts room
+    end
+end
